1520e9272b66bc65f5905d668d81f4b475dfb3a0
Dark <lookjota@gmail.com>
2023-04-05 23:57:02 -0300

    edit sidebar img

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -8,7 +8,7 @@ export function Sidebar() {
     <aside className={styles.sidebar}>
       <img
         className={styles.cover} 
-        src="https://images.unsplash.com/photo-1504006833117-8886a355efbf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60" 
+        src="https://images.unsplash.com/photo-1431440869543-efaf3388c585?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60" 
       />
 
       <div className={styles.profile}>
